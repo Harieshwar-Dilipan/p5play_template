800dc4675349354331757ae6bc3c2d0ae0f7db50
Harieshwar-Dilipan <89254307+Harieshwar-Dilipan@users.noreply.github.com>
2021-08-22 07:11:19 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -6,6 +6,17 @@ function setup() {
 function draw() 
 {
   background(30);
+  if(keyDown("w"){
+     background("red");
+}
+  if(keyDown("a"){
+     background("blue");
+}
+  if(keyDown("s"){
+     background("green");
+}
+  if(keyDown("d"){
+     background("yellow");
 
 }
 
